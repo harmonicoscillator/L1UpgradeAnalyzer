a1ed4f0b932b2bcea8062c940d31228ac2cbeb25
R. Alex Barbieri <luck@mit.edu>
2015-03-10 11:58:46 -0400

    add back a version just for HI RAW data

---
+++ test/runEmulatorAndAnalyzer_fromRAW_HIData.py
@@ -0,0 +1,56 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process('L1TEMULATION')
+
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration/StandardSequences/FrontierConditions_GlobalTag_cff')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('Configuration.Geometry.GeometryIdeal_cff')
+
+# Select the Message Logger output you would like to see:
+process.load('FWCore.MessageService.MessageLogger_cfi')
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(10)
+    )
+
+# Input source
+process.source = cms.Source("PoolSource",
+    secondaryFileNames = cms.untracked.vstring(),
+                            fileNames = cms.untracked.vstring(
+'root://xrootd.unl.edu//store/user/icali/HIHighPt/HIMinBiasUPC_Skim_HLT_HIJet55_v2/72e9032319e369ee7d3493bb45d1ee7d/SD_HIJet55_1001_1_f4s.root'
+                            )
+)
+
+process.options = cms.untracked.PSet()
+
+# Other statements
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, 'GR_P_V27A::All', '')
+process.GlobalTag.connect = cms.string('frontier://FrontierProd/CMS_COND_31X_GLOBALTAG')
+
+process.load('L1Trigger.L1TCalorimeter.L1TCaloStage1_HIFromRaw_cff')
+
+process.p1 = cms.Path(
+    process.L1TCaloStage1_HIFromRaw
+)
+
+process.TFileService = cms.Service("TFileService",
+                                   fileName = cms.string("L1UpgradeAnalyzer.root")
+)
+
+process.L1UpgradeAnalyzer = cms.EDAnalyzer('l1t::L1UpgradeAnalyzer',
+                                           InputLayer2Collection = cms.InputTag("simCaloStage1FinalDigis"),
+                                           InputLayer2TauCollection = cms.InputTag("simCaloStage1FinalDigis:rlxTaus"),
+                                           InputLayer2IsoTauCollection = cms.InputTag("simCaloStage1FinalDigis:isoTaus"),
+                                           InputLayer2CaloSpareCollection = cms.InputTag("simCaloStage1FinalDigis:HFRingSums"),
+                                           InputLayer1Collection = cms.InputTag("simRctUpgradeFormatDigis"),
+                                           legacyRCTDigis = cms.InputTag("simRctDigis")
+)
+
+process.p2 = cms.Path(process.L1UpgradeAnalyzer)
+
+process.schedule = cms.Schedule(
+    process.p1, process.p2
+    )
