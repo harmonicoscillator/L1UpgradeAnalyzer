6fc049e91195b933601ec28c2d1c16b3be0c4322
ginnocen <ginnocen@cern.ch>
2015-10-21 23:26:42 +0200

    changes for unpacking gct

--- plugins/L1UpgradeAnalyzer.h
+++ plugins/L1UpgradeAnalyzer.h
@@ -62,18 +62,6 @@ namespace l1t {
 
       // ----------member data ---------------------------
 
-
-      //edm::EDGetToken FEDRawToken_;
-      edm::EDGetToken EGammaToken_;
-      edm::EDGetToken TauToken_;
-      edm::EDGetToken isoTauToken_;
-      edm::EDGetToken JetToken_;
-      edm::EDGetToken EtSumToken_;
-      edm::EDGetToken CaloSpareToken_;
-
-      edm::EDGetToken RegionToken_;
-      edm::EDGetToken EmCandToken_;
-
       edm::EDGetToken legacyRegionToken_;
       edm::EDGetToken legacyEmCandToken_;
 
@@ -89,94 +77,6 @@ namespace l1t {
       //int FEDBXID;
       int run;
       int lumi;
-
-      int nJet;
-      int *jet_hwPt;
-      int *jet_hwEta;
-      int *jet_hwPhi;
-      int *jet_hwQual;
-      int *jet_hwIso;
-      int *jet_bx;
-
-      double *jet_pt;
-      double *jet_eta;
-      double *jet_phi;
-
-      int nTau;
-      int *tau_hwPt;
-      int *tau_hwEta;
-      int *tau_hwPhi;
-      int *tau_hwQual;
-      int *tau_hwIso;
-      int *tau_bx;
-
-      double *tau_pt;
-      double *tau_eta;
-      double *tau_phi;
-
-      int nIsotau;
-      int *isotau_hwPt;
-      int *isotau_hwEta;
-      int *isotau_hwPhi;
-      int *isotau_hwQual;
-      int *isotau_hwIso;
-      int *isotau_bx;
-
-      double *isotau_pt;
-      double *isotau_eta;
-      double *isotau_phi;
-
-      int nEgamma;
-      int *egamma_hwPt;
-      int *egamma_hwEta;
-      int *egamma_hwPhi;
-      int *egamma_hwQual;
-      int *egamma_hwIso;
-      int *egamma_bx;
-
-      double *egamma_pt;
-      double *egamma_eta;
-      double *egamma_phi;
-
-      int nEtsum;
-      int *etsum_hwPt;
-      int *etsum_hwEta;
-      int *etsum_hwPhi;
-      int *etsum_hwQual;
-      int *etsum_hwIso;
-      int *etsum_type;
-      int *etsum_bx;
-
-      double *etsum_pt;
-      double *etsum_eta;
-      double *etsum_phi;
-
-      int nHfring;
-      int *hfring_hwPt;
-      int *hfring_hwEta;
-      int *hfring_hwPhi;
-      int *hfring_hwQual;
-      int *hfring_hwIso;
-      int *hfring_bx;
-
-      double *hfring_pt;
-      double *hfring_eta;
-      double *hfring_phi;
-
-      int nRegions;
-      int *region_hwPt;
-      int *region_hwEta;
-      int *region_hwPhi;
-      int *region_tauVeto;
-      int *region_bx;
-
-      int nEMCands;
-      int *emcand_hwPt;
-      int *emcand_hwEta;
-      int *emcand_hwPhi;
-      int *emcand_hwIso;
-      int *emcand_hwQual;
-      int *emcand_bx;
 
       int nLegacyRegions;
       int *legacyregion_raw;
---
+++ test/runForest_PbPb_MIX_75X.py
@@ -0,0 +1,256 @@
+import FWCore.ParameterSet.Config as cms
+process = cms.Process('HiForest')
+process.options = cms.untracked.PSet(
+    # wantSummary = cms.untracked.bool(True)
+    #SkipEvent = cms.untracked.vstring('ProductNotFound')
+)
+
+#####################################################################################
+# HiForest labelling info
+#####################################################################################
+
+process.load("HeavyIonsAnalysis.JetAnalysis.HiForest_cff")
+process.HiForest.inputLines = cms.vstring("HiForest V3",)
+import subprocess
+version = subprocess.Popen(["(cd $CMSSW_BASE/src && git describe --tags)"], stdout=subprocess.PIPE, shell=True).stdout.read()
+if version == '':
+    version = 'no git info'
+process.HiForest.HiForestVersion = cms.untracked.string(version)
+
+#####################################################################################
+# Input source
+#####################################################################################
+
+process.source = cms.Source("PoolSource",
+                            duplicateCheckMode = cms.untracked.string("noDuplicateCheck"),
+                            fileNames = cms.untracked.vstring( "root://xrootd.unl.edu//store/user/dgulhan/Hydjet_Quenched_MinBias_5020GeV/HydjetMB_753p1_MCHI2_74_V3_53XBS_RECODEBUG/86aa60f252c71fab24f2e09dda1db221/step3_1000_1_4ZE.root"),
+                            #secondaryFileNames = cms.untracked.vstring("root://xrootd.unl.edu//store/user/mnguyen/Hydjet_Quenched_MinBias_5020GeV/HydjetMB_740pre8_MCHI2_74_V3_53XBS_DIGI-RAW/6da45e4e90741bc03dbd9aec5f36c050/step2_DIGI_L1_DIGI2RAW_HLT_RAW2DIGI_L1Reco_119_1_Pxs.root")
+
+    ) 
+
+# Number of events we want to process, -1 = all events
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(10))
+
+
+#####################################################################################
+# Load Global Tag, Geometry, etc.
+#####################################################################################
+
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('Configuration.Geometry.GeometryDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_38T_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+process.load('Configuration.StandardSequences.Digi_cff')
+process.load('Configuration.StandardSequences.SimL1Emulator_cff')
+process.load('Configuration.StandardSequences.DigiToRaw_cff')
+process.load('Configuration.StandardSequences.RawToDigi_cff')
+process.load('Configuration.StandardSequences.ReconstructionHeavyIons_cff')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+
+# PbPb 53X MC
+
+from Configuration.AlCa.GlobalTag_condDBv2 import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run2_mc_HIon', '')
+
+process.GlobalTag.toGet.extend([
+ cms.PSet(record = cms.string("HeavyIonRcd"),
+tag = cms.string("CentralityTable_HFtowers200_HydjetDrum5_v740x01_mc"),
+connect = cms.string("frontier://FrontierProd/CMS_COND_31X_PHYSICSTOOLS"),
+label = cms.untracked.string("HFtowersHydjetDrum5")
+ ),
+])
+
+from HeavyIonsAnalysis.Configuration.CommonFunctions_cff import overrideJEC_PbPb2760
+overrideJEC_PbPb2760(process)
+
+process.load("RecoHI.HiCentralityAlgos.CentralityBin_cfi")
+process.centralityBin.Centrality = cms.InputTag("hiCentrality")
+process.centralityBin.centralityVariable = cms.string("HFtowers")
+process.centralityBin.nonDefaultGlauberModel = cms.string("HydjetDrum5")
+
+#####################################################################################
+# Define tree output
+#####################################################################################
+
+process.TFileService = cms.Service("TFileService",
+                                   fileName=cms.string("HiForest.root"))
+
+#####################################################################################
+# Additional Reconstruction and Analysis: Main Body
+#####################################################################################
+
+
+process.load('HeavyIonsAnalysis.JetAnalysis.jets.akPu3CaloJetSequence_PbPb_mc_cff')
+process.load('HeavyIonsAnalysis.JetAnalysis.jets.akVs3CaloJetSequence_PbPb_mc_cff')
+process.load('HeavyIonsAnalysis.JetAnalysis.jets.akVs3PFJetSequence_PbPb_mc_cff')
+process.load('HeavyIonsAnalysis.JetAnalysis.jets.akPu3PFJetSequence_PbPb_mc_cff')
+
+process.load('HeavyIonsAnalysis.JetAnalysis.jets.akPu4CaloJetSequence_PbPb_mc_cff')
+process.load('HeavyIonsAnalysis.JetAnalysis.jets.akVs4CaloJetSequence_PbPb_mc_cff')
+process.load('HeavyIonsAnalysis.JetAnalysis.jets.akVs4PFJetSequence_PbPb_mc_cff')
+process.load('HeavyIonsAnalysis.JetAnalysis.jets.akPu4PFJetSequence_PbPb_mc_cff')
+
+process.load('HeavyIonsAnalysis.JetAnalysis.jets.akPu5CaloJetSequence_PbPb_mc_cff')
+process.load('HeavyIonsAnalysis.JetAnalysis.jets.akVs5CaloJetSequence_PbPb_mc_cff')
+process.load('HeavyIonsAnalysis.JetAnalysis.jets.akVs5PFJetSequence_PbPb_mc_cff')
+process.load('HeavyIonsAnalysis.JetAnalysis.jets.akPu5PFJetSequence_PbPb_mc_cff')
+
+process.jetSequences = cms.Sequence(process.akPu3CaloJetSequence +
+                                    process.akVs3CaloJetSequence +
+                                    process.akVs3PFJetSequence +
+                                    process.akPu3PFJetSequence +
+
+                                    process.akPu4CaloJetSequence +
+                                    process.akVs4CaloJetSequence +
+                                    process.akVs4PFJetSequence +
+                                    process.akPu4PFJetSequence
+
+                                    # process.akPu5CaloJetSequence +
+                                    # process.akVs5CaloJetSequence +
+                                    # process.akVs5PFJetSequence +
+                                    # process.akPu5PFJetSequence
+
+                                    )
+
+process.load('HeavyIonsAnalysis.EventAnalysis.hievtanalyzer_mc_cfi')
+process.hiEvtAnalyzer.doMC = cms.bool(False) #the gen info dataformat has changed in 73X, we need to update hiEvtAnalyzer code
+process.load('HeavyIonsAnalysis.EventAnalysis.hltanalysis_cff')
+process.load('HeavyIonsAnalysis.JetAnalysis.HiGenAnalyzer_cfi')
+
+#####################################################################################
+# To be cleaned
+
+process.load('HeavyIonsAnalysis.JetAnalysis.ExtraTrackReco_cff')
+process.load('HeavyIonsAnalysis.JetAnalysis.TrkAnalyzers_MC_cff')
+process.load("HeavyIonsAnalysis.TrackAnalysis.METAnalyzer_cff")
+process.load("HeavyIonsAnalysis.JetAnalysis.pfcandAnalyzer_cfi")
+process.load('HeavyIonsAnalysis.JetAnalysis.rechitanalyzer_cfi')
+process.rechitAna = cms.Sequence(process.rechitanalyzer+process.pfTowers)
+process.pfcandAnalyzer.skipCharged = False
+process.pfcandAnalyzer.pfPtMin = 0
+
+#####################################################################################
+
+#########################
+# Track Analyzer
+#########################
+process.anaTrack.qualityStrings = cms.untracked.vstring(['highPurity','tight','loose'])
+
+process.pixelTrack.qualityStrings = cms.untracked.vstring('highPurity')
+process.hiTracks.cut = cms.string('quality("highPurity")')
+
+# set track collection to iterative tracking
+process.anaTrack.trackSrc = cms.InputTag("hiGeneralTracks")
+
+# clusters missing in recodebug - to be resolved
+process.anaTrack.doPFMatching = False
+process.pixelTrack.doPFMatching = False
+
+process.anaTrack.doSimVertex = True
+process.anaTrack.doSimTrack = True
+# process.ppTrack.fillSimTrack = True
+
+process.load("SimTracker.TrackAssociation.trackingParticleRecoTrackAsssociation_cff")
+process.tpRecoAssocGeneralTracks = process.trackingParticleRecoTrackAsssociation.clone()
+process.tpRecoAssocGeneralTracks.label_tr = cms.InputTag("hiGeneralTracks")
+process.quickTrackAssociatorByHits.ComponentName = cms.string('quickTrackAssociatorByHits')
+
+
+#####################
+# Photons
+#####################
+
+process.load('HeavyIonsAnalysis.PhotonAnalysis.ggHiNtuplizer_cfi')
+process.ggHiNtuplizer.genParticleSrc = cms.InputTag("genParticles")
+process.ggHiNtuplizerGED = process.ggHiNtuplizer.clone(recoPhotonSrc = cms.InputTag('gedPhotonsTmp'),
+                                                       recoPhotonHiIsolationMap = cms.InputTag('photonIsolationHIProducerGED')
+                                                       )
+
+#####################
+
+# HYDJET RECO file didn't have ak2GenJets and ak6GenJets as input, so removed them
+# and ran our own hiGenJets sequence
+# from RecoHI.HiJetAlgos.HiGenJets_cff import ak3HiGenJets, ak4HiGenJets
+# from RecoJets.Configuration.GenJetParticles_cff import genParticlesForJets
+# genParticlesForJets.ignoreParticleIDs += cms.vuint32( 12,14,16)
+
+# process.hiSelectGenJets = cms.Sequence(
+#     genParticlesForJets +
+#     ak3HiGenJets +
+#     ak4HiGenJets
+# )
+
+process.anaTrack.doSimTrack = cms.untracked.bool(False)
+
+process.HiGenParticleAna.genParticleSrc = cms.untracked.InputTag("genParticles")
+
+process.load("GeneratorInterface.HiGenCommon.HeavyIon_cff")
+ 
+process.load("EventFilter.L1TRawToDigi.caloStage1Digis_cfi")
+process.caloStage1Digis.InputLabel = cms.InputTag("rawDataCollector")
+
+process.dumpRaw = cms.EDAnalyzer( 
+    "DumpFEDRawDataProduct",
+    label = cms.untracked.string("rawDataCollector"),
+    feds = cms.untracked.vint32 ( 1352 ),
+    dumpPayload = cms.untracked.bool (True)
+)
+
+
+
+process.ana_step = cms.Path(process.heavyIon*
+                            process.hltanalysis *
+#temp                            process.hltobject *
+                            process.centralityBin *
+                            process.hiEvtAnalyzer*
+                            process.HiGenParticleAna*
+                            #process.hiGenJetsCleaned*
+                            process.quickTrackAssociatorByHits*
+                            #process.tpRecoAssocGeneralTracks + #used in HiPFJetAnalyzer
+                            #process.hiSelectGenJets +
+                            process.jetSequences +
+                            process.ggHiNtuplizer +
+                            process.ggHiNtuplizerGED +
+                            process.pfcandAnalyzer +
+                            process.rechitAna +
+                            process.HiForest +
+                            # process.cutsTPForFak +
+                            # process.cutsTPForEff +
+                            process.anaTrack
+                            #process.pixelTrack
+                            )
+
+process.gctDigis = cms.EDProducer("GctRawToDigi",
+    checkHeaders = cms.untracked.bool(False),
+    gctFedId = cms.untracked.int32(745),
+    hltMode = cms.bool(False),
+    inputLabel = cms.InputTag("rawDataCollector"),
+    numberOfGctSamplesToUnpack = cms.uint32(5),
+    numberOfRctSamplesToUnpack = cms.uint32(1),
+    unpackSharedRegions = cms.bool(False),
+    unpackerVersion = cms.uint32(0),
+    verbose = cms.untracked.bool(True)
+)
+
+process.myResults = cms.EDAnalyzer('l1t::L1UpgradeAnalyzer',
+                                         legacyRCTDigis = cms.InputTag("gctDigis")
+)
+
+process.p2 = cms.Path(process.gctDigis+process.myResults)
+
+
+process.load('HeavyIonsAnalysis.JetAnalysis.EventSelection_cff')
+process.phltJetHI = cms.Path( process.hltJetHI )
+process.pcollisionEventSelection = cms.Path(process.collisionEventSelection)
+process.load('CommonTools.RecoAlgos.HBHENoiseFilterResultProducer_cfi')
+process.pHBHENoiseFilterResultProducer = cms.Path( process.HBHENoiseFilterResultProducer )
+process.phfCoincFilter = cms.Path(process.hfCoincFilter )
+process.phfCoincFilter3 = cms.Path(process.hfCoincFilter3 )
+process.pprimaryVertexFilter = cms.Path(process.primaryVertexFilter )
+process.phltPixelClusterShapeFilter = cms.Path(process.siPixelRecHits*process.hltPixelClusterShapeFilter )
+process.phiEcalRecHitSpikeFilter = cms.Path(process.hiEcalRecHitSpikeFilter )
+
+process.pAna = cms.EndPath(process.skimanalysis)
+
+# Customization
